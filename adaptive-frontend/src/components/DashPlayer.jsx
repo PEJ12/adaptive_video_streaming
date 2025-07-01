429561fc9f8df02eff617ad0981896b17027fea9
PEJ12 <dmswo5677@naver.com>
2025-07-01 17:45:31 +0900

    DashPlayer clenup에 이전 인스턴스 정리

--- adaptive-frontend/src/components/DashPlayer.jsx
+++ adaptive-frontend/src/components/DashPlayer.jsx
@@ -24,7 +24,14 @@ export default function DashPlayer({ manifestUrl }) {
       })
 
     return () => {
-      if (player) player.reset()
+      if (player) {
+      player.reset()
+      player = null              // Dash.js 인스턴스 해제
+    }
+    if (videoRef.current) {
+      videoRef.current.removeAttribute('src')  
+      videoRef.current.load()     // video 태그의 src 언로드
+    }
     }
   }, [manifestUrl])
 
@@ -38,68 +45,3 @@ export default function DashPlayer({ manifestUrl }) {
     </div>
   )
 }
-
-/*
-import React, { useRef, useEffect } from 'react'
-//import dashjs from 'dashjs'
-import dashjs from 'dashjs/dist/modern/esm/dash.all.min.js'
-import './DashPlayer.css'           // CSS는 여기서 import
-
-export default function DashPlayer({ manifestUrl }) {
-  const videoRef = useRef(null)
-
-  useEffect(() => {
-    // ← 여기에 꼭 로그를 남겨서 초기화가 호출되는지 봅니다.
-    console.log('DashPlayer init with URL:', manifestUrl)
-
-    if (!videoRef.current) return
-    const player = dashjs.MediaPlayer().create()
-    player.initialize(videoRef.current, manifestUrl, true)
-    return () => player.reset()
-  }, [manifestUrl])
-
-  return (
-    <div className="dash-player-container">
-      <video
-        ref={videoRef}
-        className="dash-player-video"
-        controls
-      />
-    </div>
-  )
-}
-*/
-
-
-/*
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import vids from '../assets/videos.js'
-import DashPlayer from '../components/DashPlayer'
-//import './PlayerPage.css'
-import './DashPlayer.css'
-
-export default function PlayerPage() {
-  const { profileId, videoId } = useParams()
-
-  // videoId는 문자열이니까 숫자로 바꿔서 조회
-  const video = vids.find(v => v.id === parseInt(videoId, 10))
-
-  if (!video) {
-    return <div className="player-page--error">⚠️ 해당 영상을 찾을 수 없습니다.</div>
-  }
-
-  return (
-    <div className="player-page">
-      <h2 className="player-page__title">
-        {video.title} — Profile {profileId}님을 위한 스트리밍
-      </h2>
-
-      { DashPlayer에 manifest URL만 넘겨주면 dash.js가 adaptive streaming 해줍니다.*}
-      <div className="player-page__player">
-        <DashPlayer manifestUrl={video.manifest} />
-      </div>
-    </div>
-  )
-}
-*/
