00b57c9411ca8c19403217fcd5acd486ad4e65e0
PEJ12 <dmswo5677@naver.com>
2025-09-09 21:46:05 +0900

    encode17.py 최종

--- Netflix/src/assets/videos.js
+++ Netflix/src/assets/videos.js
@@ -6,19 +6,19 @@ export default [
     id: 1,
     title: '영상 1',
     // 서버에서 제공하는 MPD manifest 경로
-    manifest: 'http://localhost:8000/stream/husky/manifest.mpd',
+    manifest: 'http://localhost:8000/stream/husky_encode13/mp4box/manifest.mpd', 
     thumb: '/assets/thumbs/video1.png', // 썸네일 이미지 경로
   },
   {
     id: 2,
     title: '영상 2',
-    manifest: 'http://localhost:8000/stream/Sports/manifest.mpd',
+    manifest: 'http://localhost:8000/stream/Sports/mp4box/manifest.mpd',
     thumb: '/assets/thumbs/video2.png',
   },
   {
     id: 3,
     title: '영상 3',
-    manifest: 'http://localhost:8000/stream/news_v2/manifest.mpd',
+    manifest: 'http://localhost:8000/stream/news_v2/mp4box/manifest.mpd',
     thumb: '/assets/thumbs/video3.png',
   }
 ]
